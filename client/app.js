64aaf0df931c83fd56a5b1b5bef62dce52931576
Erin Kavanaugh <elkavanaugh@gmail.com>
2016-05-25 20:49:55 -0700

    (feat) Initial webpack configuration and test files

---
+++ client/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Greeter from './Greeter';
+
+import '../public/styles.css';
+
+render(<Greeter />, document.getElementById('root'));
