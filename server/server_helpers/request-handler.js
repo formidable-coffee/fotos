351817ed9b8dcca26ffe5f2e1b5c3b2c41849dba
Erin Kavanaugh <elkavanaugh@gmail.com>
2016-05-26 17:08:20 -0700

    Fixed path for routes

--- server/server_helpers/request-handler.js
+++ server/server_helpers/request-handler.js
@@ -1,13 +1,15 @@
-// Get /
-// POST /signin
-// GET /create
-// POST /create
 var bodyParser = require('body-parser');
+var path = require('path')
 
+module.exports.main = {
+	get: function (req, res) {
+		res.redirect('/signin'); 
+	}
+}; 
 
 module.exports.signin = {
 	get: function (req, res) {
-		res.send('success'); 
+		res.sendFile(path.normalize(__dirname + '/../../public/sign-in.html')); 
 	}, 
 
 	post: function (req, res) {
