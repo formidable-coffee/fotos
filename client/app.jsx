ca9b398de1a501dfb30798e46e2933f0952af0ed
Grisanu Naing <mgrisanu@gmail.com>
2016-05-27 11:51:14 -0700

    (refactor) Factored Reactjs code from html to jsx

--- client/app.jsx
+++ client/app.jsx
@@ -1,1 +1,7 @@
+import { Button } from 'react-bootstrap';
+
+ReactDOM.render(
+    <Button bsStyle="danger" bsSize="large">Sign In</Button>, 
+    document.getElementsByClassName('signIn')[0]);
+
 console.log('I am not alive'); 
