e61c5b3408cdf81f45735641ed1e5a339020cd5d
Adam Lessen <lessena@gmail.com>
2016-05-27 20:30:10 -0700

    (cleanup) more clean up

--- client/arc.jsx
+++ client/arc.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; 
 import Lightbox from 'react-images';
-console.log('should be a function: ', Lightbox);
+
 
 class Arc extends React.Component {
   constructor(props) {
