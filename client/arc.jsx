94233c77c79cf3a62fadc7e290d1173172c4e9e5
Adam Lessen <lessena@gmail.com>
2016-05-27 20:03:23 -0700

    Implement photo gallery using light box
    - Include react-images in package.json
    - Main component rendered in arc for now

---
+++ client/arc.jsx
@@ -0,0 +1,139 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+import Lightbox from 'react-images';
+console.log('should be a function: ', Lightbox);
+
+class Arc extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('props.photoArc', this.props.photoArc);
+    console.log('props.photoArc mapped', this.props.photoArc.map(photo => photo.src));
+
+    this.state = {
+      lightboxIsOpen: false,
+      currentImage: 0,
+    };
+    this.closeLightbox = this.closeLightbox.bind(this);
+    this.gotoNext = this.gotoNext.bind(this);
+    this.gotoPrevious = this.gotoPrevious.bind(this);
+    this.handleClickImage = this.handleClickImage.bind(this);
+    this.openLightbox = this.openLightbox.bind(this);
+  }
+
+  openLightbox (index, event) {
+    event.preventDefault();
+    this.setState({
+      currentImage: index,
+      lightboxIsOpen: true,
+    });
+  }
+  closeLightbox () {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false,
+    });
+  }
+  gotoPrevious () {
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    });
+  }
+  gotoNext () {
+    this.setState({
+      currentImage: this.state.currentImage + 1,
+    });
+  }
+  handleClickImage () {
+    if (this.state   === this.props.images.length - 1) return;
+
+    this.gotoNext();
+  }
+
+
+  renderGallery () {
+    if (!this.props.photoArc) return;
+    const gallery = this.props.photoArc.map((obj, i) => {
+      return (
+        <a
+          href={obj.src}
+          key={i}
+          onClick={(e) => this.openLightbox(i, e)}
+          style={styles.thumbnail}
+          >
+          <img
+            height={styles.thumbnail.size}
+            src={obj.thumbnail}
+            style={styles.thumbnailImage}
+            width={styles.thumbnail.size}
+          />
+        </a>
+      );
+    });
+
+    return (
+      <div style={styles.gallery}>
+        {gallery}
+      </div>
+    );
+  }
+  
+    render () {
+      return (
+        <div className="section">
+          {this.renderGallery()}
+          <Lightbox
+            currentImage={this.state.currentImage}
+            images={this.props.photoArc}
+            isOpen={this.state.lightboxIsOpen}
+            onClickPrev={this.gotoPrevious}
+            onClickNext={this.gotoNext}
+            onClickImage={this.handleClickImage}
+            onClose={this.closeLightbox}
+          />
+        </div>
+    );
+  }
+
+};
+
+// PropTypes tell other developers what `props` a component expects
+// Warnings will be shown in the console when the defined rules are violated
+Arc.propTypes = {
+  photoArc: React.PropTypes.array.isRequired
+};
+
+const THUMBNAIL_SIZE = 72;
+
+const styles = {
+  gallery: {
+    marginLeft: -5,
+    marginRight: -5,
+    overflow: 'hidden',
+  },
+  thumbnail: {
+    backgroundSize: 'cover',
+    borderRadius: 3,
+    float: 'left',
+    height: THUMBNAIL_SIZE,
+    margin: 5,
+    overflow: 'hidden',
+    width: THUMBNAIL_SIZE,
+  },
+  thumbnailImage: {
+    display: 'block',
+    height: 'auto',
+    maxWidth: '100%',
+    // height: THUMBNAIL_SIZE,
+    // left: '50%',
+    // position: 'relative',
+    //
+    // WebkitTransform: 'translateX(-50%)',
+    // MozTransform:    'translateX(-50%)',
+    // msTransform:     'translateX(-50%)',
+    // transform:       'translateX(-50%)',
+  },
+};
+
+
+
+export {Arc};
---
+++ client/feed.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+import {Arc} from './arc'
+
+// expecting to be passed an array of urls in props
+
+var Feed = (props) => (
+  <div className="feed">
+    i am the feed
+    <Arc photoArc={props.arcs} />
+  </div>
+);
+
+// PropTypes tell other developers what `props` a component expects
+// Warnings will be shown in the console when the defined rules are violated
+Feed.propTypes = {
+  arcs: React.PropTypes.array.isRequired
+};
+
+export {Feed};
