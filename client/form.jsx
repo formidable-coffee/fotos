ad1e929885c1986d4fcaac87b1fc285f2d31d6d5
Erin Kavanaugh <elkavanaugh@gmail.com>
2016-06-06 13:46:56 -0700

    API request to facebook returns photos filtered by dates

--- client/form.jsx
+++ client/form.jsx
@@ -16,25 +16,57 @@ class Form extends React.Component {
 	}
   
   submitHandler (startDate, endDate, options) {
-    $.post({
-      url: '/create',
-      data: {
-        startDate: startDate,
-        endDate: endDate,
-        // options: options
-      },
-      success: function() {
-        console.log('success'); 
+
+    console.log(startDate, endDate, 'in submit handler'); 
+    var getPhotos = function (response) {
+      for (element in response.data) {
+        post = response.data[element]; 
+        console.log(post.id+ ": " + post.message); 
       }
-    });
+
+      if (i < 2) {
+        nextPage = response.paging.next;
+        console.log(nextPage);
+        i++;
+        $.get(nextPage, getPosts, 'json'); 
+      }
+    }; 
+    FB.api('me/photos?fields=images,created_time&until='+endDate+'&since='+startDate, function (response) {
+      console.log(response); 
+      $.post({
+        url: '/create',
+        data: response,
+        success: function() {
+          console.log('success'); 
+        }
+      });
+    })
+    // FB.api('/me/photos', {since: startDate, until: endDate}, function (response) {
+    //   console.log(response); 
+    //   $.post({
+    //     url: '/create',
+    //     data: {
+    //       startDate: startDate,
+    //       endDate: endDate,
+    //       // options: options
+    //     },
+    //     success: function() {
+    //       console.log('success'); 
+    //     }
+    //   });
+    // })
   }
 
 	handleSubmit (e) {
 		e.preventDefault();
 		console.log("start is", this.state.startDate); 
 		console.log("end is", this.state.endDate); 
+    var self = this; 
+    console.log(Date.parse(this.state.startDate));    // endDate = dateChange(endDate); 
 
-		this.submitHandler(this.state.startDate, this.state.endDate);
+		// this.submitHandler(Date.parse(self.state.startDate), Date.parse(self.state.endDate));
+    this.submitHandler(self.state.startDate, self.state.endDate);
+
 	}
 
 	dropdownSelect (e) {
